a86b42468e28f2e0a23b4417890313ad5b9571ac
araf95 <you@example.com>
2023-11-19 00:12:16 +0600

    Class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Hello'
+import Porjotok from './components/porjotok'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         
         <Welcome/>
+        <Porjotok/>
       </header>
     </div>
   );
---
+++ src/components/porjotok.js
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react'
+
+class Porjotok extends Component{
+	render(){
+		return <h2> Please Visit www.porjotok.xyz </h2>
+	}
+
+}
+
+export default Porjotok
