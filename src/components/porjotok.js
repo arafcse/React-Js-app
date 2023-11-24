77caa05c0870b9d830f45be7c4976f83ebd64ebd
araf95 <you@example.com>
2023-11-24 19:55:58 +0600

    onClick funcion

--- src/components/porjotok.js
+++ src/components/porjotok.js
@@ -6,17 +6,22 @@ class Porjotok extends Component{
 	this.state = {
 		website:[
 
-			{name:'PORJOTOK.xyz',founder:'Araf Ahmed'},
-			{name:'shop.xyz',founder:'Araf Ahmed'},
+			{name:'PORJOTOK.xyz',founder:'Araf'},
+			{name:'shop.xyz',founder:'Ahmed'},
 			]
 		}
 	}
+clickHandler = () => {
+	console.log('Hello ARAF');
+	alert('Welcome PORJOTOK')
+}
+
 	render(){
 		
 		return ( 
 			<div>
+				<button onClick={this.clickHandler}>Click Here</button>
 			<h2> Please Visit {this.state.website[0].name} Founder {this.state.website[1].founder}</h2>
-				<h2> Please Visit {this.state.website[1].name} Founder {this.state.website[1].founder}</h2>
 				</div>
 			)
 	}
