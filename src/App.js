0dcf78ecfbdd5e4b5673050d294186cb8e425325
araf95 <you@example.com>
2023-11-30 20:28:19 +0600

    login form with validation

--- src/App.js
+++ src/App.js
@@ -3,38 +3,52 @@ import React from "react";
 class App extends React.Component{
 
 state = {
-  username:"",
-  phonenumber:"",
-  email:"",
-  city:""
 
+  email:"",
+  password:"",
+  emailError:'',
+  passError:''
+
+}
+
+validate(){
+  if(!this.state.email>0 && !this.state.password>0){
+    this.setState({emailError:"Email and Password Required"})
+
+  }else if(!this.state.email>0){
+    this.setState({emailError:"Email field cannot be blank"})
+
+  }else if(!this.state.password>0){
+
+    this.setState({passError:"Password field cannot be blank"})
+  }else{
+    return true;
+  }
 }
   
-
-ChangeData = (event) => {
-  let val = event.target.name;
-  let myvalue = event.target.value
-  this.setState({[val]:myvalue});
+formSubmit = (e) => {
+  e.preventDefault();
+  if(this.validate()){
+    alert("Form Submit Done");
+  }
 }
-
 
 
   render(){
     return(
       <div className="App">
-        <h1>{this.state.username} </h1>
-        <h1>{this.state.phonenumber} </h1>
-        <h1>{this.state.email} </h1>
-        <h1>{this.state.city} </h1>
+        <h1>Login Panel</h1>
+        
         <div>
-          <form>
+          <form onSubmit={this.formSubmit}>
 
             <div class="form-group" style={{marginLeft:200}}>
-              <label>User Name:</label><br></br>
-              <input type="text" name="username" onChange={this.ChangeData}></input><br></br>
-              <input type="text" name="phonenumber" onChange={this.ChangeData}></input><br></br>
-              <input type="text" name="email" onChange={this.ChangeData}></input><br></br>
-              <input type="text" name="city" onChange={this.ChangeData}></input><br></br>
+              <label>Email:</label><br></br>
+              <input type="email" name="email" onChange={(event)=>{this.setState({email:event.target.value})}}></input><br></br>
+              <strong style={{color:'red'}}>{this.state.emailError}</strong><br></br>
+              <label>Password:</label><br></br>
+              <input type="password" name="password" onChange={(event)=>{this.setState({password:event.target.value})}}></input><br></br>
+              <strong style={{color:'red'}}>{this.state.passError}</strong><br></br>
               <button type="submit">Submit</button>
 
 
